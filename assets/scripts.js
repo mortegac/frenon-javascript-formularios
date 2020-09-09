279f7bf63b7c0a3c78441f95dcd309e06a940d83
Raul Ruiz <vrap3@hotmail.com>
2020-09-09 15:36:27 -0300

    agregar y borrar registro

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,44 @@
 const saveUser = (event) => {
     event.preventDefault() // Para que no se recargue la pagina y se haga el POST
-    console.log("click en boton")
-
-    //Rescatar valor de campo correo
 
 
+    //Rescatar valores
     var name = document.getElementById("name").value
-    var correo = document.getElementById("email").value
+    var email = document.getElementById("email").value
+    var valueUsers = [{
+        name,
+        email
+    }]
 
-    console.log("valor de name", name)
-    console.log("valor de correo", correo)
-
-
-
-    // Rescatar valor de campo nombre 
     // Almacenar en el localStorage
+    localStorage.setItem("Users", JSON.stringify(valueUsers))
+    printUsers()
 
+}
+const printUsers = () => {
+    var Users = JSON.parse(localStorage.getItem("Users"))
+
+    for (let index = 0; index < Users.length; index++) {
+        console.log(Users[index])
+        var liElement = document.createElement("li");
+        var att = document.createAttribute("id");
+        att.value = Users[index].name;
+        liElement.setAttributeNode(att);
+        liElement.innerHTML = `
+            <a href="#" onclick="handleClick(event)">
+            <ion-icon name="trash-outline"></ion-icon>
+            ${Users[index].name}( ${Users[index].email})
+            </a>`
+        document.getElementById("list").appendChild(liElement)
+    }
+}
+
+
+const handleClick = (event) => {
+    var Users = JSON.parse(localStorage.getItem("Users"))
+
+    event.target.parentNode.remove()
+
+
+    // localStorage.removeItem("")
 }
